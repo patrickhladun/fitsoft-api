d4ca157b691910f53302146aa569817a40dec635
Patrick <patrickhladun@gmail.com>
2021-06-05 13:34:28 +0100

    Create basic CRUD operations for exercises

--- routes/exercise.routes.js
+++ routes/exercise.routes.js
@@ -1,9 +1,21 @@
 const express = require('express');
-const { addExercise } = require('../controllers/exercise');
+const { addExercise, updateExercise, getExercises, deleteExercise } = require('../controllers/exercise');
 const router = express.Router();
 
 router.post('/exercise',
     addExercise
+);
+
+router.post('/exercise/:id',
+    updateExercise
+);
+
+router.get('/exercises/',
+    getExercises
+);
+
+router.delete('/exercise/:id',
+    deleteExercise
 );
 
 module.exports = router;
